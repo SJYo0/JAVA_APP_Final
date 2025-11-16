8f1cd5874e946e9186d068a4f09f8f9004359170
SJYo0 <wnsdyd9109@gmail.com>
2025-11-16 19:32:53 +0900

    맵 수정

--- src/Model/MapObject/gameMap.java
+++ src/Model/MapObject/gameMap.java
@@ -13,9 +13,9 @@ public class gameMap {
         lands[1] = new Land(500,780,300,30);
         lands[2] = new Land(100,660,300,30);
         lands[3] = new Land(500,540,300,30);
-        lands[4] = new Land(900,300,100,90);
+        lands[4] = new Land(900,150,100,90);
         lands[5] = new Land(1100,540,300,30);
-        lands[6] = new Land(1500,300,100,90);
+        lands[6] = new Land(1500,150,100,90);
         lands[7] = new Land(1700,540,300,30);
 
         map.add(lands);
